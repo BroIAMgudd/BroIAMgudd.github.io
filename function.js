69caff6b7b18ad9d324521be028d58b2fd24b259
HiGuyss <65045058+HiGuyss@users.noreply.github.com>
2020-09-15 14:00:06 -0500

    Update function.js

--- function.js
+++ function.js
@@ -1,1 +1,25 @@
-wow
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementsByTagName('form')[0].onsubmit = function(evt) {
+    evt.preventDefault();
+    checkWord();
+    window.scrollTo(0, 150);
+  }
+  document.getElementById('terminalTextInput').focus();
+  var textInputValue = document.getElementById('terminalTextInput').value.trim();
+  // spelling error check later with css
+  var textInputValue = document.getElementById('terminalResultsCont').innerHTML;
+  var clearInput = function(){
+    document.getElementById('terminalTextInput').value = "";
+  }
+  var scrollToBottomOfResults = function() {
+    //another spelling error
+    var terminalResultsDiv = document.getElemenetById('terminalResultsCont');
+    terminalResultsDiv.scrollTop = terminalResultsDiv.scrollHeight;
+  }
+  scrollToBottomOfResults();
+  var addTextToResults = function(textToAdd) {
+    //another one
+    document.getElementById('terminalResultsCont').innerHTML +- "<p>" + textToAdd + "</p>";
+    scrollToBottomOfResults();
+  }
+}
